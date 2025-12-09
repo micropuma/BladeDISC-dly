f20722d3ae346f2e63eb116205ba46a8d1764b7e
micropuma.github.io <micropuma@163.com>
2025-12-09 16:42:33 +0000

    [chore] upgrade tensorrt from 8.6 to 10.11

--- config.sh
+++ config.sh
@@ -1,7 +1,7 @@
 # tensorrt & cudnn环境
 unset LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=/usr/local/cuda/lib64:/usr/local/nvidia/lib:/usr/local/nvidia/lib64:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=/system/TensorRT-8.6.1.6/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/system/TensorRT-10.11.0.33/lib:$LD_LIBRARY_PATH
 
 # hugging face镜像  
 export HF_ENDPOINT=https://hf-mirror.com
