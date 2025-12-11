88109f34c824e83b1da1790c4740c7e3bd53ce08
micropuma.github.io <micropuma@163.com>
2025-12-11 14:24:23 +0000

    [chore] a better code to dump

--- tutorial/PyTorch/quick-start.py
+++ tutorial/PyTorch/quick-start.py
@@ -1,22 +1,20 @@
 import torch
 import torch_blade
 
-class MyCell(torch.nn.Module):
-    def __init__(self):
-        super(MyCell, self).__init__()
-        self.c = torch.randn(10, 3)
-
-    def forward(self, x, y):
-        t1 = x + y
-        t2 = torch.matmul(t1, self.c)
-        t3 = torch.sum(t2)
-        return t3 * t3
-
-my_cell = MyCell()
-x = torch.rand(10, 10)
-y = torch.rand(10, 10)
-
+import os
+# set TORCH_BLADE_DEBUG_LOG=on
+os.environ["TORCH_BLADE_DEBUG_LOG"] = "on"
+# do BladeDISC optimization
+w = h = 10
+dnn = torch.nn.Sequential(
+      torch.nn.Linear(w, h),
+      torch.nn.ReLU(),
+      torch.nn.Linear(h, w),
+      torch.nn.ReLU()).cuda().eval()
 with torch.no_grad():
-    blade_cell = torch_blade.optimize(my_cell, allow_tracing=True, model_inputs=(x, y))
+  # BladeDISC torch_blade optimize will return an optimized TorchScript
+  opt_dnn_ts = torch_blade.optimize(
+    dnn, allow_tracing=True, model_inputs=(torch.ones(w, h).cuda(),))
 
-print(blade_cell(x, y))
+# print optimized code
+print(opt_dnn_ts.code)
